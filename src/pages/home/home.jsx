fefcbf823bc6b8e49369d1554d20e92e471cc1f4
Muzaffar2903 <muzaffar2903@gmail.com>
2022-01-18 19:13:17 +0500

    Finished App

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,24 @@
+import Chart from "../../components/chart/Chart";
+import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
+import "./home.css";
+import { userData } from "../../dummyData";
+import WidgetLg from "../../components/widgetLg/widgetLg";
+import WidgetSm from "../../components/widgetSm/widgetSm";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <FeaturedInfo />
+      <Chart
+        data={userData}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
+      <div className="homeWidgets">
+        <WidgetSm />
+        <WidgetLg />
+      </div>
+    </div>
+  );
+}
